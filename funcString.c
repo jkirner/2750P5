bfc79a02bab4fa4ff26a45d1f78da20c2dbcbd77
jkirner <33811924+jkirner@users.noreply.github.com>
2017-11-19 13:20:31 -0600

    Create funcString.c

---
+++ funcString.c
@@ -0,0 +1,19 @@
+//Jonathan Kirner
+//cs2750 PA5
+//11/19/17
+
+#include <studio.h>
+#include <string.h>
+#include "funcString.h"
+
+void funcString(char* subStr, int start, int size, char *str[]){
+  int i=start+1;
+  char *ptr;
+  while(i<size){
+    ptr=strstr(str[i], subStr)
+    if(ptr!=null){
+      printf("%s is a substring of %s", subStr, str[i]);
+    }
+    i++;
+  }
+}
